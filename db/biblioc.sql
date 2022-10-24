449eb00796ba07536182a919d0139f0ae04f99ee
Yiber Eulices López Choconta <115713084+YelcoBot@users.noreply.github.com>
2022-10-24 15:18:43 -0500

    db

--- db/biblioc.sql
+++ db/biblioc.sql
@@ -1,10 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.29, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
 -- Host: localhost    Database: biblioteca
 -- ------------------------------------------------------
--- Server version	8.0.30-0ubuntu0.22.04.1
-
-USE biblioteca;
+-- Server version	8.0.31
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -25,46 +23,50 @@ DROP TABLE IF EXISTS `autor`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `autor` (
-  `cod_autor` int NOT NULL AUTO_INCREMENT,
-  `nombre` int NOT NULL,
-  PRIMARY KEY (`cod_autor`),
-  KEY `nombre` (`nombre`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(75) CHARACTER SET utf8mb3 COLLATE utf8mb3_spanish2_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nombre_UNIQUE` (`nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `categorias`
+-- Dumping data for table `autor`
 --
 
-DROP TABLE IF EXISTS `categorias`;
+LOCK TABLES `autor` WRITE;
+/*!40000 ALTER TABLE `autor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `autor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categoria`
+--
+
+DROP TABLE IF EXISTS `categoria`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `categorias` (
-  `id_categoria` int NOT NULL AUTO_INCREMENT,
-  `nombre` int NOT NULL,
-  PRIMARY KEY (`id_categoria`),
+CREATE TABLE `categoria` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_spanish2_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nombre_UNIQUE` (`nombre`),
   KEY `nombre` (`nombre`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `consulta_libro`
+-- Dumping data for table `categoria`
 --
 
-DROP TABLE IF EXISTS `consulta_libro`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `consulta_libro` (
-  `id_consulta` int NOT NULL AUTO_INCREMENT,
-  `cod_libro` int NOT NULL,
-  `id_usu` int NOT NULL,
-  PRIMARY KEY (`id_consulta`),
-  KEY `cod_libro` (`cod_libro`,`id_usu`),
-  KEY `id_usu` (`id_usu`),
-  CONSTRAINT `consulta_libro_ibfk_1` FOREIGN KEY (`cod_libro`) REFERENCES `libro` (`cod_libro`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `consulta_libro_ibfk_2` FOREIGN KEY (`id_usu`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `categoria` WRITE;
+/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `editorial`
@@ -74,56 +76,24 @@ DROP TABLE IF EXISTS `editorial`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `editorial` (
-  `cod_editorial` int NOT NULL AUTO_INCREMENT,
-  `nombre` int NOT NULL,
-  PRIMARY KEY (`cod_editorial`),
-  KEY `cod_editorial` (`nombre`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(75) CHARACTER SET utf8mb3 COLLATE utf8mb3_spanish2_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nombre_UNIQUE` (`nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `libro`
+-- Dumping data for table `editorial`
 --
 
-DROP TABLE IF EXISTS `libro`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `libro` (
-  `cod_libro` int NOT NULL AUTO_INCREMENT,
-  `titulo` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_spanish2_ci NOT NULL,
-  `n_pag` int NOT NULL,
-  `cod_volumen` int NOT NULL,
-  `año_edicion` varchar(10) CHARACTER SET utf8mb3 COLLATE utf8mb3_spanish2_ci NOT NULL,
-  `cod_editorial` int NOT NULL,
-  `id_categoria` int NOT NULL,
-  PRIMARY KEY (`cod_libro`),
-  KEY `cod_editorial` (`cod_editorial`,`id_categoria`),
-  KEY `id_categoria` (`id_categoria`),
-  KEY `cod_volumen` (`cod_volumen`),
-  CONSTRAINT `libro_ibfk_2` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id_categoria`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `libro_ibfk_3` FOREIGN KEY (`cod_editorial`) REFERENCES `editorial` (`cod_editorial`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `libro_ibfk_4` FOREIGN KEY (`cod_volumen`) REFERENCES `volumen` (`cod_volumen`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `libro_autor`
---
-
-DROP TABLE IF EXISTS `libro_autor`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `libro_autor` (
-  `cod_libro_autor` int NOT NULL AUTO_INCREMENT,
-  `cod_autor` int NOT NULL,
-  `cod_libro` int NOT NULL,
-  PRIMARY KEY (`cod_libro_autor`),
-  KEY `cod_autor` (`cod_libro`),
-  KEY `cod_autor_2` (`cod_autor`),
-  CONSTRAINT `libro_autor_ibfk_1` FOREIGN KEY (`cod_libro`) REFERENCES `libro` (`cod_libro`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `libro_autor_ibfk_2` FOREIGN KEY (`cod_autor`) REFERENCES `autor` (`cod_autor`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `editorial` WRITE;
+/*!40000 ALTER TABLE `editorial` DISABLE KEYS */;
+INSERT INTO `editorial` VALUES (1,'Edit1','2022-10-24 17:43:19','2022-10-24 17:43:19');
+/*!40000 ALTER TABLE `editorial` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `rol`
@@ -133,11 +103,25 @@ DROP TABLE IF EXISTS `rol`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `rol` (
-  `id_rol` int NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_spanish2_ci NOT NULL,
-  PRIMARY KEY (`id_rol`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
+  `estado` binary(1) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nombre_UNIQUE` (`nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rol`
+--
+
+LOCK TABLES `rol` WRITE;
+/*!40000 ALTER TABLE `rol` DISABLE KEYS */;
+INSERT INTO `rol` VALUES (2,'Administrador',_binary '1','2022-10-24 20:24:38','2022-10-24 21:12:08');
+/*!40000 ALTER TABLE `rol` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
@@ -152,14 +136,25 @@ CREATE TABLE `user` (
   `apellido` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
   `password` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
   `email` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-  `remember_token` text COLLATE utf8mb3_spanish2_ci,
+  `estado` binary(1) NOT NULL,
+  `remember_token` text CHARACTER SET utf8mb3 COLLATE utf8mb3_spanish2_ci,
   `timestamp` time DEFAULT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (5,'Yiber Eulices','Lopez Choconta','$2y$10$cgtGQRDEreR0xqBsNLcy6uwMcj2UlYyhAsY94pt1LHnMiuElQd9wW','yelcobot@gmail.com',_binary '1','hdfpETSzpqW6lQSH55WBz2w02oHbyyudLlyB7sIMQk3aPFFGuC7UySPl5HB0',NULL,'2022-10-19 20:54:46','2022-10-24 21:17:07'),(8,'Asbleidy Katherin','Alba Lopez','$2y$10$TSEaSG4MawLNSk4I1lqBFe8UnlKBqcRh0U0WxZH.SqL/10d.zp3J.','Asbleidy295@gmail.com',_binary '\0',NULL,NULL,'2022-10-19 23:20:07','2022-10-19 23:20:07'),(10,'Jesus','Lopez','$2y$10$OrDj4d5TyMYdyzM3JhpeLet1qdT2XgxqyeKPG0QykyhbyN87f0iXO','jesus@gmail.com',_binary '\0',NULL,NULL,'2022-10-20 16:08:50','2022-10-20 16:08:50');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `volumen`
@@ -176,6 +171,15 @@ CREATE TABLE `volumen` (
   KEY `cod_libro` (`cod_libro`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `volumen`
+--
+
+LOCK TABLES `volumen` WRITE;
+/*!40000 ALTER TABLE `volumen` DISABLE KEYS */;
+/*!40000 ALTER TABLE `volumen` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -186,4 +190,4 @@ CREATE TABLE `volumen` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-10-19  8:22:09
+-- Dump completed on 2022-10-24 15:18:14
